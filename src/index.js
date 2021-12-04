f4bf756ce3b81d7ecff6be8da9ab15092dc8c384
whoguy24 <whoguy24@gmail.com>
2021-12-04 10:18:22 -0600

    Rejiggered reducers to one reducer to manage complexity, and built feedBackReducer set values logic.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-// INITIALIZE REACT, REDUX, APP AND OTHER NONSENSE
+///////////////////////////////////////////////////////
+/// INITIALIZE REACT, REDUX, APP AND OTHER NONSENSE ///
+///////////////////////////////////////////////////////
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -8,35 +11,46 @@ import { createStore, combineReducers, applyMiddleware  } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
-// CREATE REDUCERS
+///////////////////////
+/// CREATE REDUCERS ///
+///////////////////////
 
-// Feeling Rating Reducer
-const feelingRating = (state = {}, action) => {
-    return state;
+// Feedback Survey Reducer
+// feedback = { feeling: '4', understanding: '2', support: '5', comments: 'Things are A-OK! 👌' }
+const feedbackReducer = (state = {}, action) => {
+
+    // Build new feedback object
+    const updatedFeedback = {...state};
+
+    // Update property based the desired action
+    switch (action.type) {
+        case 'SET_FEELING':
+            updatedFeedback.feeling = action.payload;
+            return  updatedFeedback;
+        case 'SET_UNDERSTANDING':
+            updatedFeedback.understanding = action.payload;
+            return  updatedFeedback;
+        case 'SET_SUPPORT':
+            updatedFeedback.support = action.payload;
+            return  updatedFeedback;
+        case 'SET_COMMENTS':
+            updatedFeedback.comments = action.payload;
+            return  updatedFeedback;
+        default:
+            return state;
+    }
+
 }
 
-// Feeling Rating Reducer
-const supportRating = (state = {}, action) => {
-    return state;
-}
+//////////////////////////
+/// CREATE REDUX STORE ///
+//////////////////////////
 
-// Feeling Rating Reducer
-const understandingRating = (state = {}, action) => {
-    return state;
-}
-
-// Feeling Rating Reducer
-const comments = (state = {}, action) => {
-    return state;
-}
-
-// Create Redux Store
+// Combine Reducers into Store
+// Apply Redux Logger
 const storeInstance = createStore(
     combineReducers({
-        feelingRating,
-        supportRating,
-        understandingRating,
-        comments
+        feedbackReducer
     }),
     applyMiddleware( logger )
 );
